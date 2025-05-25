e2c358f0dc79cd8187969b186c08a816ece89113
Vasia Rapti <v_rapti@yahoo.com>
2025-05-26 00:51:17 +0300

    configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import staticAdapter from '@astrojs/adapter-static';
 
 export default defineConfig({
   integrations: [
