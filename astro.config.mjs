98df47839f0d9c86cc7401e14ac9423e0dec52fc
Vasia Rapti <60875684+vasiarapti@users.noreply.github.com>
2025-05-26 13:16:18 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 // import staticAdapter from '@astrojs/adapter-static';
-import github from '@astrojs/github';
+// import github from '@astrojs/github';
 
 const LIVE_URL = "https://vasiarapti.github.io/";
 
