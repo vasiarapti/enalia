192a41b2944f940a5527645a51a13c7ff10f81ba
Vasia Rapti <60875684+vasiarapti@users.noreply.github.com>
2025-06-04 08:53:11 +0300

    bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export default {
     },
     backgroundImage: {
       'waves': "url('../assets/layered-waves-haikei.svg')",
-      'background': "url('../assets/stacked-waves-haikei.svg')",
+      'background': "url('../assets/wave-haikei.svg')",
     },
   },
   plugins: [],
