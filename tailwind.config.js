4e7a1b4b1cfdc8022e73ebc4d772bceb06a6732c
Vasia Rapti <60875684+vasiarapti@users.noreply.github.com>
2025-06-04 12:05:03 +0300

    banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}'],
   theme: {
     extend: {
+      animation: {
+        marquee: 'marquee 20s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'tramslateX(-100%)' },
+        },
+      },
       colors: {
         primary: '#769d88',        // Sidebar background
         hover: '#617d6e',          // Hover color
