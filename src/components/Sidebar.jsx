cb1a2fd41e6ff4458e0d2c0492ceff66a40e4a14
Vasia Rapti <60875684+vasiarapti@users.noreply.github.com>
2025-06-05 13:19:53 +0300

    sidebar mobile

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -54,10 +54,10 @@ export default function Sidebar() {
       {/* Sidebar */}
       <aside
         className={`
-          fixed top-0 left-0 w-72 h-screen bg-primary text-secondary z-40 p-6 overflow-y-auto
+          fixed top-0 left-0 h-screen bg-primary text-secondary z-40 p-6 overflow-y-auto
     transform transition-transform duration-300
     ${isOpen ? "translate-x-0" : "-translate-x-full"} 
-    md:translate-x-0 md:transform-none
+    w-full md:w-72 md:translate-x-0 md:transform-none
         `}
       >
         {/* Logo */}
