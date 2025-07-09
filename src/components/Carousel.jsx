5387b1b9ff72ce4e19532a15a386837ff7178e15
Vasia Rapti <60875684+vasiarapti@users.noreply.github.com>
2025-07-09 18:57:19 +0300

    our-place

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,6 +2,9 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import place1 from "../assets/our-place/image1.JPG";
+import place2 from '../assets/our-place/image2.JPG';
+import place3 from '../assets/our-place/image3.jpg';
 
 export default function ImageCarousel() {
   return (
@@ -17,13 +20,26 @@ export default function ImageCarousel() {
         emulateTouch
       >
         <div>
-          <img src="/images/our-place/images1.jpg" alt="Therapy room 1" />
+          {/* <img src="/images/our-place/image1.jpg" alt="Therapy room 1" /> */}
+          {/* <Image src={place1} alt="Χώρος"/> */}
+          <img
+            src={place1.src}
+            alt="Χώρος"
+          />
         </div>
         <div>
-          <img src="/images/our-place/images2.jpg" alt="Therapy room 2" />
+          {/* <img src="/images/our-place/image2.jpg" alt="Therapy room 2" /> */}
+          <img
+            src={place2.src}
+            alt="Χώρος"
+          />
         </div>
         <div>
-          <img src="/images/our-place/images3.jpg" alt="Therapy room 3" />
+          {/* <img src="/images/our-place/image3.jpg" alt="Therapy room 3" /> */}
+          <img
+            src={place3.src}
+            alt="Χώρος"
+          />
         </div>
       </Carousel>
     </div>
