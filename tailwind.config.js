4d092e770e5e2291ee8aa4dbefa96ecb65cf3a54
Vasia Rapti <60875684+vasiarapti@users.noreply.github.com>
2025-08-31 16:41:48 +0300

    last fix for dropdown

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import flowbitePlugin from 'flowbite/plugin';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -28,7 +30,5 @@ export default {
       'background': "url('../assets/wave-haikei.svg')",
     },
   },
-  plugins: [
-    [flowbite]
-  ],
+  plugins: [flowbitePlugin]
 };
