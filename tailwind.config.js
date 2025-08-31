c21925b3fbdc363eca18346263a9f1c0eba04ed0
Vasia Rapti <60875684+vasiarapti@users.noreply.github.com>
2025-08-31 16:23:19 +0300

    fix contact dropdown

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}',
+    './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}',
     './node_modules/flowbite/**/*.js',
   ],
   theme: {
